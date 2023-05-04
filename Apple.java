ef7714a8d8da163d870006f05d68705802a3e207
mzqan <123510256+mzqan@users.noreply.github.com>
2023-05-04 09:15:10 -0400

    Implemented gameOver() method
    1.9 Game Over:
    Remove apple and draws game over when apple reaches bottom
    Added false param to the world so objects aren't bounded to its height

--- Apple.java
+++ Apple.java
@@ -19,5 +19,12 @@ public class Apple extends Actor
         int y = getY() + 2;
         setLocation(x, y);
         
+        //Remove apple and draw game over when apple gets to bottom 
+        MyWorld world = (MyWorld) getWorld();
+        if(getY() <= world.getHeight()) 
+        {
+            world.gameOver();
+            world.removeObject(this);
+        }
     }
 }
