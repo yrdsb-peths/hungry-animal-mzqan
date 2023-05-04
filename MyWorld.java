329f57e903f920244005a404c9ff674b3596798f
mzqan <123510256+mzqan@users.noreply.github.com>
2023-05-04 08:34:07 -0400

    1.3  Reading Greenfoot API
    Added Apple.java
    Added apple object in code
    Apple falls downwards

--- MyWorld.java
+++ MyWorld.java
@@ -19,6 +19,9 @@ public class MyWorld extends World
         super(600, 400, 1);
         
         Elephant elephant = new Elephant();
-        addObject(elephant, 300, 300);
+        addObject(elephant, 300, 200);
+        
+        Apple apple = new Apple();
+        addObject(apple, 300, 0);
     }
 }
