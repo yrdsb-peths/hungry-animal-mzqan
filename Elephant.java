81547b3639de5848219f0c6f42093e611b4ac40d
mzqan <123510256+mzqan@users.noreply.github.com>
2023-05-10 17:37:36 -0400

    Added elephant sound
    2.2

--- Elephant.java
+++ Elephant.java
@@ -11,6 +11,8 @@ public class Elephant extends Actor{
      * Act - do whatever the Elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
     public void act() {
         //Add your action code here.
         if(Greenfoot.isKeyDown("left"))
@@ -34,6 +36,7 @@ public class Elephant extends Actor{
             MyWorld world = (MyWorld) getWorld();
             world.createApple();
             world.increaseScore();
+            elephantSound.play();
         }
     }     
 }
