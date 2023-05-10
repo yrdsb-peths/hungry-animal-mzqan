05527cfb7a59ad6d05c1458ac5c8fbb891a9378b
mzqan <123510256+mzqan@users.noreply.github.com>
2023-05-10 18:03:13 -0400

    Added SimpleTimer

--- Elephant.java
+++ Elephant.java
@@ -18,6 +18,7 @@ public class Elephant extends Actor{
     
     //Direction the elephant is facing
     String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
     
     public Elephant () {
         for(int i = 0; i < idleRight.length;i++) {
@@ -30,6 +31,9 @@ public class Elephant extends Actor{
             idleLeft[i].mirrorHorizontally();
             idleLeft[i ].scale(100,100);
         }
+        
+        animationTimer.mark(); 
+        
         //Initial elephant image
         setImage(idleRight[0]);
     }
@@ -39,6 +43,10 @@ public class Elephant extends Actor{
      */
     int imageIndex = 0;
     public void animateElephant() {
+        if(animationTimer.millisElapsed() < 100) {
+            return;
+        }
+        animationTimer.mark();
         if(facing.equals("right")) {
             setImage(idleRight[imageIndex]);
             imageIndex = (imageIndex + 1) % idleRight.length;
