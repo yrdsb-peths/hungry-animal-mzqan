e9e65e9c3a9698aa793617fbd0f0b870aa08ffd6
mzqan <123510256+mzqan@users.noreply.github.com>
2023-05-12 16:50:25 -0400

    Added prepare() method

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Hungry Elephant", 60);
+    Label titleLabel = new Label("The Elephant", 60);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -18,7 +18,8 @@ public class TitleScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+        addObject(titleLabel, getWidth()/2, 200);
+        prepare();
     }
     /**
      * The main world act loop
@@ -28,5 +29,25 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program 
+     * That is: create the initial objects and add them to the world.
+     */
+    public void prepare() {
+        Elephant elephant = new Elephant();
+        addObject(elephant, 479, 87);
+        elephant.setLocation(479, 120); 
+        Label label = new Label("Use \u2190 and \u2192 to Move", 40);
+        addObject(label, 220, 257); 
+        label.setLocation(297, 243); 
+        label.setLocation(305, 240);
+        Label label2 = new Label ("Press <space> to Start", 40); 
+        addObject(label2, 249, 331); 
+        label2.setLocation(326, 319); 
+        label.setLocation(328, 253); 
+        label2.setLocation(300,326); 
+        label.setLocation(300,261);
     }
 }
